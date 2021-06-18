5f18d91352c2fb73ecee3d90a59c54ba6c0e0a4f
zloop1995 <zloop1995@gmail.com>
2021-06-18 17:56:33 -0500

    New components
    new components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
-  <hello-world />
+  <Login />
 </template>
 
 <script>
   import HelloWorld from '../components/HelloWorld'
+  import Login from '../components/Login'
 
   export default {
     name: 'Home',
 
     components: {
       HelloWorld,
+      Login,
     },
   }
 </script>
