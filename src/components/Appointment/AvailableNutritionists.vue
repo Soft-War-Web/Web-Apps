0147fa0b76bbbbae7672bc2347e053c48e803a39
Josue <josue.c1012@gmail.com>
2021-07-01 23:54:21 -0500

    appointments

--- src/components/Appointment/AvailableNutritionists.vue
+++ src/components/Appointment/AvailableNutritionists.vue
@@ -1,10 +1,9 @@
 <template>
 <div style=";width: 100% ">
-    <template >
-      <v-toolbar flat color="white">
+      <v-toolbar flat color="blue">
         <v-toolbar-title>Nutricionistas disponibles</v-toolbar-title>
       </v-toolbar>
-    </template >
+    <br>
     <div class="d-flex flex-wrap  justify-space-between">
     <v-card class="mx-auto mb-10" max-width="400" v-for="(nutritionist,i) in nutritionists" :key="i" >
     <v-img class="white--text align-end" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
@@ -25,19 +24,15 @@
     </v-card-text>
 
     <v-card-actions>
-      <v-btn color="orange" to="/nutritionistSelected"> Agendar </v-btn>
-      <v-btn to="/nutritionistSelected"> Nose </v-btn>
+      <v-btn to="/nutritionistSelected"> Agendar </v-btn>
     </v-card-actions>
   </v-card>
   </div>
 </div>
 </template>
 
-<nutritionistSelected: clientId="clientId" @send=""/>
-
 <script>
 import axios from 'axios'
-import { nutritionistSelected } from "./NutritionistSelected.vue";
 export default {
     data: () => ({
         dialog: false,
@@ -51,41 +46,24 @@ export default {
         createdAt: '',    
         nutritionists: [],
         loading: false,
-        return: {
-            clientId: 1
-            }
     }),
-        created () {
-            this.list();
-        },
+    created () {
+      this.list();
+      },
     methods: {
       reserve () {
         this.loading = true
         setTimeout(() => (this.loading = false), 2000)
       },
       list(){
-                let me = this;
-                axios.get('api/Nutritionists')
-                .then(function(response){
-                  me.nutritionists = response.data;
-                  console.log(response.data);
-                }).catch(function(error){
-                  console.log(error);
-                });
-            },
-            deleteItem (item) {
-              let me = this;
-              if(confirm('¿Estás seguro que quieres eliminar este Category?'))
-                axios.delete('api/Nutritionists/'+item.nutritionistId,{
-                      'id': item.nutritionistId
-                }).then(function(response){
-                  console.log(item.id);
-                  me.close();
-                  me.list();
-                  me.clean();
-                }).catch(function(error){
-                  console.log(error);
-                });
+        let me = this;
+        axios.get('api/Nutritionists')
+        .then(function(response){
+          me.nutritionists = response.data;
+          console.log(response.data);
+          }).catch(function(error){
+            console.log(error);
+            });
             },
             getNutritionistById(){
                 axios.get('api/Nutritionists/GetNutritionistById/'+this.nutritionistId)
@@ -94,47 +72,7 @@ export default {
                 }).catch(function(error){
                   console.log(error);
                 });
-            },            
-            save() {
-              let me = this;
-              this.getNutritionistById(this.nutritionistId);
-              if(this.editedIndex > -1) { //Editar Category
-                axios.put('api/Nutritionists/PutNutritionist',{
-                           'nutritionistId': me.id,
-                          'username': me.username,
-                          'password': me.password,
-                          'firstName': me.firstName,
-                          'lastName': me.lastName,
-                          'email': me.email,
-                          'cnpNumber':me.cnpNumber,
-                          'createdAt': me.createdAt
-                }).then(function(response){
-                  me.close();
-                  me.list();
-                  me.clean();
-                }).catch(function(error){
-                  console.log(error);
-                });
-              } else{
-                axios.post('api/Nutritionists',{ // Nuevo Category
-                         'username': me.username,
-                          'password': me.password,
-                          'firstName': me.firstName,
-                          'lastName': me.lastName,
-                          'email': me.email,
-                          'cnpNumber':me.cnpNumber,
-                          'createdAt': me.createdAt
-                          
-                }).then(function(response){
-                  me.close();
-                  me.list();
-                  me.clean();
-                }).catch(function(error){
-                  console.log(error);
-                });
-            }
-            this.close()
-          },
+            },     
     },
   }
 </script>
--- src/components/Appointment/NutritionistSelected.vue
+++ src/components/Appointment/NutritionistSelected.vue
@@ -1,110 +1,107 @@
 <template>
-<v-btn block>
-    {{clientId}}
-</v-btn>
+<div style=";width: 100% ">
+  <v-toolbar flat color="blue">
+    <v-toolbar-title>Nutricionista seleccionado</v-toolbar-title>
+  </v-toolbar>
+  <br>
+  <div class="d-flex flex-wrap  left-space-between">
+    <v-card max-width="400">
+    <v-img class="white--text align-end" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+      <v-card-title>{{username}}</v-card-title>
+    </v-img>
+    <v-card-subtitle class="pb-0">Nombre</v-card-subtitle>
+    <v-card-text class="text--primary">
+      <div>{{firstName+' '+lastName}}</div>
+    </v-card-text>
+    <v-card-subtitle class="pb-0">Email</v-card-subtitle>
+    <v-card-text class="text--primary">
+      <div>{{email}}</div>
+    </v-card-text>
+    <v-card-subtitle class="pb-0">Número CNP</v-card-subtitle>
+    <v-card-text class="text--primary">
+      <div>{{cnpNumber}}</div>
+    </v-card-text>
+  </v-card>
+  <div class="d-flex flex-wrap  justify-space-between mx-auto mb-2">
+    <v-btn text @click="submit">{{appointmentDate}}</v-btn>
+    <v-btn text @click="submit">{{appointmentDate}}</v-btn>
+    <v-btn text @click="submit">{{appointmentDate}}</v-btn>
+    <v-btn text @click="submit">{{appointmentDate}}</v-btn>
+  </div>
+  <div class="d-flex flex-wrap  left-space-between mx-auto mb-8">
+ 
+  </div>
+  </div>
+</div>
 </template>
 
 <script>
 import axios from 'axios'
   export default {
     data: () => ({
-        dialog: false,
             id: '',
             username: '',
             firstName: '',
             lastName: '',
             email: '',
             cnpNumber:'',
-            createdAt: '',    
-            nutritionist: '',
-            props: [
-                clientId
-            ],
+            nutritionistId: 1,
+            clientId: 1,
+            nutritionistNotes: '',
             loading: false,
+            appointmentDate: new Date(),
+            amount: 35,
+            ruc: 12345,
+            billDate: new Date()
     }),
-        created () {
-            this.list();
-        },
+    created () 
+    {
+      this.getNutritionist();
+    },
     methods: {
-      reserve () {
+      reserve () 
+      {
         this.loading = true
         setTimeout(() => (this.loading = false), 2000)
       },
-      list(){
-                let me = this;
-                axios.get('api/Nutritionists')
-                .then(function(response){
-                  me.nutritionists = response.data;
-                  console.log(response.data);
-                }).catch(function(error){
-                  console.log(error);
-                });
-            },
-            metodo(){
-                alert('a');
-            },
-            deleteItem (item) {
-              let me = this;
-              if(confirm('¿Estás seguro que quieres eliminar este Category?'))
-                axios.delete('api/Nutritionists/'+item.nutritionistId,{
-                      'id': item.nutritionistId
-                }).then(function(response){
-                  console.log(item.id);
-                  me.close();
-                  me.list();
-                  me.clean();
-                }).catch(function(error){
-                  console.log(error);
-                });
-            },
-            getNutritionistById(){
-                axios.get('api/Nutritionists/GetNutritionistById/'+this.nutritionistId)
-                .then(function(response){
-                  this.nutritionistId=response.data.nutritionistId;
-                }).catch(function(error){
-                  console.log(error);
-                });
-            },            
-            save() {
-              let me = this;
-              this.getNutritionistById(this.nutritionistId);
-              if(this.editedIndex > -1) { //Editar Category
-                axios.put('api/Nutritionists/PutNutritionist',{
-                           'nutritionistId': me.id,
-                          'username': me.username,
-                          'password': me.password,
-                          'firstName': me.firstName,
-                          'lastName': me.lastName,
-                          'email': me.email,
-                          'cnpNumber':me.cnpNumber,
-                          'createdAt': me.createdAt
-                }).then(function(response){
-                  me.close();
-                  me.list();
-                  me.clean();
-                }).catch(function(error){
-                  console.log(error);
-                });
-              } else{
-                axios.post('api/Nutritionists',{ // Nuevo Category
-                         'username': me.username,
-                          'password': me.password,
-                          'firstName': me.firstName,
-                          'lastName': me.lastName,
-                          'email': me.email,
-                          'cnpNumber':me.cnpNumber,
-                          'createdAt': me.createdAt
-                          
-                }).then(function(response){
-                  me.close();
-                  me.list();
-                  me.clean();
-                }).catch(function(error){
-                  console.log(error);
-                });
-            }
-            this.close()
-          },
+      getNutritionist()
+      {
+        let me = this;
+        axios.get('api/Nutritionists/GetNutritionistById/'+this.nutritionistId)
+        .then(function(response){
+          me.nutritionistId = response.data.nutritionistId;
+          me.username = response.data.username;
+          me.firstName = response.data.firstName;
+          me.lastName = response.data.lastName;
+          me.cnpNumber = response.data.cnpNumber;
+          me.email = response.data.email;
+          }).catch(function(error){
+            console.log(error);
+            });
+      },
+      submit()
+      {
+        try
+        {
+          let me = this;
+          axios.post('api/Appointments',{
+            'clientId': me.clientId,
+            'nutritionistId': me.nutritionistId,
+            'appointmentDate': me.appointmentDate,
+            'nutritionistNotes': me.nutritionistNotes,
+            })
+        }
+        finally
+        {
+          let me = this;
+          axios.post('api/Bills',{
+            'clientId': me.clientId,
+            'amount': me.amount,
+            'ruc': me.ruc,
+            'billDate': me.billDate,
+            })
+        }
+      }
     },
   }
 </script>
